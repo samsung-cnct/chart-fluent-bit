acd83cfafbefc4d61baabf6c00ed56af57ea5041
guineveresaenger <guineveresaenger@gmail.com>
2017-10-30 15:59:40 -0700

    added artifacts, tests fail if chart_name does not exist

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,7 @@
 CHART_VER=$(git describe --tags --abbrev=0 2>/dev/null | sed 's/^v//')
 CHART_REL=$(git rev-list --count v${CHART_VER}..HEAD 2>/dev/null )
 
+mkdir -p ${chart_name}
+
 CHART_VER=${CHART_VER:-0.0.1} CHART_REL=${CHART_REL:-0} \
     envsubst < build/Chart.yaml.in > ${chart_name}/Chart.yaml
